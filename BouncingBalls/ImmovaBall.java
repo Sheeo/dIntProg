5ff4580eb9041bfef786cefe6f1455c2f3871cd9
Michael Søndergaard <sheeo@sheeo.dk>
2010-09-13 20:50:25 +0200

    Add ImmovaBall

---
+++ BouncingBalls/ImmovaBall.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ImmovaBall here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ImmovaBall  extends Ball
+{
+    /**
+     * Act - do whatever the ImmovaBall wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+		
+    }    
+}
