8491dbd4ec58c333ea37afcd1f39cedcb51aaaad
Mathias Rav <mathiasrav@gmail.com>
2010-09-15 15:05:47 +0200

    [BouncingBalls] merge VectorActor into ShapeActor

--- BouncingBalls/ShapeActor.java
+++ BouncingBalls/ShapeActor.java
@@ -1,7 +1,17 @@
-abstract class ShapeActor extends VectorActor {
+import greenfoot.*;
+abstract class ShapeActor extends Actor {
 	abstract boolean intersects(ShapeActor other);
 	protected Vector size;
 	Vector getSize() {return size;}
 	int getWidth() {return (int) Math.round(size.x());}
 	int getHeight() {return (int) Math.round(size.y());}
+	public Vector getLocation() {
+		return new Vector(getX(), getY());
+	}
+	public void setLocation(Vector vector) {
+		setLocation((int) Math.round(vector.x()), (int) Math.round(vector.y()));
+	}
+	public void moveBy(Vector vector) {
+		setLocation(getLocation().add(vector));
+	}
 }
