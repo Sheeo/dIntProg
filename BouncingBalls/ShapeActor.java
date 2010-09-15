cae158c63a02e35152101e9cac4fa87584664b43
Mathias Rav <mathiasrav@gmail.com>
2010-09-15 18:17:53 +0200

    [BouncingBalls] ShapeActor: Fix code style, make getLocation() update the internal vector if the position has changed from the outside somehow

--- BouncingBalls/ShapeActor.java
+++ BouncingBalls/ShapeActor.java
@@ -1,14 +1,22 @@
 import greenfoot.*;
 abstract class ShapeActor extends Actor {
 	abstract Shape getShape();
-	boolean intersects(ShapeActor other) {return getShape().intersects(other.getShape());}
 	protected Vector size;
 	private Vector location;
-	Vector getSize() {return size;}
-	int getWidth() {return (int) Math.round(size.x());}
-	int getHeight() {return (int) Math.round(size.y());}
+	boolean intersects(ShapeActor other) {
+		return getShape().intersects(other.getShape());
+	}
+	Vector getSize() {
+		return size;
+	}
+	int getWidth() {
+		return (int) Math.round(size.x());
+	}
+	int getHeight() {
+		return (int) Math.round(size.y());
+	}
 	public Vector getLocation() {
-		if (location == null) {
+		if (location == null || getX() != (int) Math.round(location.x()) || getY() != (int) Math.round(location.y())) {
 			location = new Vector(getX(), getY());
 		}
 		return location;
