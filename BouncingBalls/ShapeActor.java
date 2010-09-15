56c5789cf13ebdfdd12eb081f88f7e2eacd50a42
Mathias Rav <mathiasrav@gmail.com>
2010-09-15 17:43:50 +0200

    [BouncingBalls] location now stored as a vector in ShapeActor to make subpixel movement possible

--- BouncingBalls/ShapeActor.java
+++ BouncingBalls/ShapeActor.java
@@ -3,13 +3,18 @@ abstract class ShapeActor extends Actor {
 	abstract Shape getShape();
 	boolean intersects(ShapeActor other) {return getShape().intersects(other.getShape());}
 	protected Vector size;
+	private Vector location;
 	Vector getSize() {return size;}
 	int getWidth() {return (int) Math.round(size.x());}
 	int getHeight() {return (int) Math.round(size.y());}
 	public Vector getLocation() {
-		return new Vector(getX(), getY());
+		if (location == null) {
+			location = new Vector(getX(), getY());
+		}
+		return location;
 	}
 	public void setLocation(Vector vector) {
+		location = vector;
 		setLocation((int) Math.round(vector.x()), (int) Math.round(vector.y()));
 	}
 	public void moveBy(Vector vector) {
