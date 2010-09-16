d8c951bcaaa464efb16f5d9c981996a7d4a07b8b
Mathias Rav <mathiasrav@gmail.com>
2010-09-16 16:11:40 +0200

    [BouncingBalls] Add PlayerPaddle and update PaddleWorld

--- BouncingBalls/PaddleWorld.java
+++ BouncingBalls/PaddleWorld.java
@@ -1,7 +1,7 @@
 public class PaddleWorld extends BallWorld {
 	public PaddleWorld() {
 		super();
-		Paddle paddle = new Paddle();
+		Paddle paddle = new PlayerPaddle("a", "d");
 		addObject(paddle, getWidth()/2, 4*getHeight()/5);
 		for (double x = ballRadius; x < getWidth()-ballRadius; x += 2*ballRadius) {
 			addBall((int) Math.round(x), 50, 0.0, 6.0);
---
+++ BouncingBalls/PlayerPaddle.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import greenfoot.*;
+public class PlayerPaddle extends Paddle {
+	public enum Controls {LEFT, RIGHT};
+	private EnumMap<Controls, String> controls;
+	public PlayerPaddle() {
+		controls = new EnumMap<Controls, String>(Controls.class);
+	}
+	public PlayerPaddle(String left, String right) {
+		this();
+		setKey(Controls.LEFT, left);
+		setKey(Controls.RIGHT, right);
+	}
+	public void setKey(Controls control, String key) {
+		controls.put(control, key);
+	}
+	public void act() {
+		super.act();
+		String keyleft = controls.get(Controls.LEFT);
+		String keyright = controls.get(Controls.RIGHT);
+		boolean left = (keyleft != null && Greenfoot.isKeyDown(keyleft));
+		boolean right = (keyright != null && Greenfoot.isKeyDown(keyright));
+		if (left && !right) {
+			setVelocity(new Vector(-2.0, 0.0));
+		} else if (right && !left) {
+			setVelocity(new Vector(2.0, 0.0));
+		}
+	}
+}
