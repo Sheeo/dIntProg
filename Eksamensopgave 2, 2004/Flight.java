d9f21fdfb08b4f7e0d1b655341b6e9705124bde7
Michael Søndergaard <msond@cs.au.dk>
2010-09-20 22:03:47 +0200

    [Eksamensopgave 2, 2004] Add flight.java

---
+++ Eksamensopgave 2, 2004/Flight.java
@@ -0,0 +1,20 @@
+public class Flight
+{
+	String flightNumber;
+	String departure;
+	String destination;
+	int totalLoadCapacity;
+	
+	void Flight(String flightNumber, String departure, String destination, int totalLoadCapacity)
+	{
+		this.flightNumber = flightNumber;
+		this.departure = departure;
+		this.destination = destination;
+		this.totalLoadCapacity = totalLoadCapacity;
+	}
+	
+	public void toString()
+	{
+		return 
+	}
+}
