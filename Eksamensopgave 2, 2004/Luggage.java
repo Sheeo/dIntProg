e4e8b8f9459acaae9058f4ea0d3729bea4b3b485
Michael Søndergaard <msond@cs.au.dk>
2010-09-23 10:25:48 +0200

    [Eksamensopgave] Add Luggage.java

---
+++ Eksamensopgave 2, 2004/Luggage.java
@@ -0,0 +1,14 @@
+public class Luggage
+{
+
+    private int weight;
+    
+    public void setWeight(int w) {
+        weight = w;
+    }
+
+    public int getWeight()
+    {
+        return weight;
+    }
+}
