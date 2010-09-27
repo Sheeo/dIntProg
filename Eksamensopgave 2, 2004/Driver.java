540617b19c2c53a1a71c38c9b0bec151049f0f0d
Michael Søndergaard <msond@cs.au.dk>
2010-09-27 10:29:27 +0200

    [Eksamensopgave 2, 2004] Implement Luggage in Flight

--- Eksamensopgave 2, 2004/Driver.java
+++ Eksamensopgave 2, 2004/Driver.java
@@ -1,7 +1,7 @@
 public class Driver
 {
-    public static void Main() {
-        Flight f = new Flight();
-        f.toString();
+    public static void Exam() {
+        Flight f = new Flight("SK 945", "CPH", "AMD", 4500);
+        System.out.println(f.toString());
     }
 }
--- Eksamensopgave 2, 2004/Flight.java
+++ Eksamensopgave 2, 2004/Flight.java
@@ -1,20 +1,43 @@
+import java.util.ArrayList;
+
 public class Flight
 {
 	String flightNumber;
 	String departure;
 	String destination;
 	int totalLoadCapacity;
-	
-	void Flight(String flightNumber, String departure, String destination, int totalLoadCapacity)
+	ArrayList<Luggage> luggage;
+
+	public Flight(String flightNumber, String departure, String destination, int totalLoadCapacity)
 	{
 		this.flightNumber = flightNumber;
 		this.departure = departure;
 		this.destination = destination;
 		this.totalLoadCapacity = totalLoadCapacity;
+		this.luggage = new ArrayList<Luggage>();
 	}
 	
-	public void toString()
+	public void addLuggage(Luggage l)
 	{
-		return 
+		luggage.add(l);
+	}
+	
+	public void removeLuggage(Luggage l)
+	{
+		luggage.remove(l);
+	}
+	
+	int availableLoadCapacity() {
+		return totalLoadCapacity - totalLoad();
+	}
+	
+	int totalLoad() {
+		return 50;
+	}
+
+	public String toString()
+	{
+		String format = "%s: %s - %s (C: %d kg)";
+		return String.format(format, flightNumber, departure, destination, totalLoadCapacity);
 	}
 }
